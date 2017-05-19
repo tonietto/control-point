e07b156a2b43d7fae2254c8b7f9189bf6c9af516
Gustavo Tonietto <tonietto@gmail.com>
2017-05-18 22:29:04 -0300

    :lipstick: Materialize
    App and storybook

--- src/materialUi/MaterialUI.js
+++ src/materialUi/MaterialUI.js
@@ -1,16 +1,14 @@
 import React from 'react';
+
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import RaisedButton from 'material-ui/RaisedButton';
 
 
-import logo from './logo.svg';
-import './App.css';
-
-
-const App = () => (
+const MaterialUI = () => (
   <MuiThemeProvider>
     <RaisedButton label="Default"/>
   </MuiThemeProvider>
 );
 
-export default App;
+export default MaterialUI;
+
