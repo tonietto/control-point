440332a8ac8a2369726bce12ad09830a6e35e222
Gustavo Tonietto <tonietto@gmail.com>
2017-05-23 21:05:31 -0300

    :lipstick: Update App component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import React from 'react';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import RaisedButton from 'material-ui/RaisedButton';
 
 
 const App = () => (
-  <MuiThemeProvider>
-    <RaisedButton label="Default" />
-  </MuiThemeProvider>
+  <h1>App</h1>
 );
 
 export default App;
